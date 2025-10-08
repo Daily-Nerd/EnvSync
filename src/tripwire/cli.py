997dc509a4b2d1e8e9d4141b1d70ce592da722ef
Kibukx <alangil505@gmail.com>
2025-10-08 17:57:30 -0600

    Bump version to 0.2.0-rc3 (prerelease)

--- src/tripwire/cli.py
+++ src/tripwire/cli.py
@@ -102,7 +102,7 @@ def print_help_with_banner(ctx, param, value):
     callback=print_help_with_banner,
     help="Show this message and exit.",
 )
-@click.version_option(version="0.2.0-rc2", prog_name="tripwire", message=f"{LOGO_SIMPLE}\nVersion: %(version)s")
+@click.version_option(version="0.2.0-rc3", prog_name="tripwire", message=f"{LOGO_SIMPLE}\nVersion: %(version)s")
 def main() -> None:
     """TripWire - Catch config errors before they explode.
 
